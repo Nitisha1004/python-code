874b1189e2e00e13298735904efbabf2fcda7510
nitish1004 <nitisha1004@gmail.com>
2023-06-24 22:49:39 +0530

    first commit

---
+++ square.py
@@ -0,0 +1,8 @@
+# import library math
+import math
+# enter the value by the user
+x=int(input("Enter the  number for which you have to calculate square root : "))
+#then calculate square root of given value
+sqr=math.sqrt(x)
+# then print square root value
+print(sqr)
