f41caf8c7f6c954d8e3cbda72e9508525a23e9f0
NITISHA KESHARWANI <114598363+Nitisha1004@users.noreply.github.com>
2023-07-01 23:32:54 +0530

    Add files via upload

---
+++ samplecode.py
@@ -0,0 +1,11 @@
+# firstname=input("Enter your first name : ")
+# lastname=input("Enter your last name: ")
+# age=int(input("Enter your age : "))
+# new=age+1
+# print("Hello "+firstname+" "+lastname +" Next year yow will be "+str(new))
+
+operand1=659
+operand2=569
+print(operand1==operand2)
+print(operand1!=operand2)
+print(operand1>=operand2)
